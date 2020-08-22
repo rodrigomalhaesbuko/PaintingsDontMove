3f9eba7680a32c733cfc4f33ebb352fb805592f2
asdasdasd <asdasdsda>
2020-08-22 17:11:50 -0300

    GameOver Method

---
+++ PaintingsDontMove/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/* Para usar a classe GameManger sem criar um referencia forte utilize o método
+ * FindObjectOfType<GameManager>();
+ */
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public float gameOverDelay = 1f;
+     
+    public void EndGame()
+    {
+        if (!gameHasEnded)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game Over");
+            Invoke("GameOver", gameOverDelay);
+            // Chamar tela de Game Over
+        }
+    }
+
+    void GameOver()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
