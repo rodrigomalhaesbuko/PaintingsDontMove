b908cedfa730536afcc1e50198d25857401d874a
HandelSM <hscholze@hotmail.com>
2020-08-22 21:20:31 -0300

    merge Quimico e Mark/added hyerogurifu

--- PaintingsDontMove/Assets/Scripts/Controllers/PlayerController.cs
+++ PaintingsDontMove/Assets/Scripts/Controllers/PlayerController.cs
@@ -7,8 +7,16 @@ public class PlayerController : MonoBehaviour
     public float dist;
     private GameObject attackLeft;
     private GameObject attackRight;
+
+    private Rigidbody rb;
+    private bool isDead = false;
+
+    private int xPosition = 0;
+    private int yPosition = 1;
+
     private void Start()
     {
+        rb = GetComponent<Rigidbody>();
         attackLeft = transform.GetChild(1).gameObject;
         attackRight = transform.GetChild(0).gameObject;
 
@@ -17,38 +25,51 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown("up"))
         {
-            Vector2 pos = transform.position;
-            pos.y += dist;
-            transform.position = pos; 
+            if (yPosition != 2)
+            {
+                transform.position = transform.position + new Vector3(0, 5, 0);
+                yPosition += 1;
+
+            }
         }
 
         if (Input.GetKeyDown("down"))
         {
-            Vector2 pos = transform.position;
-            pos.y -= dist;
-            transform.position = pos;
+            if (yPosition != 0)
+            {
+                transform.position = transform.position + new Vector3(0, -5, 0);
+                yPosition -= 1;
+
+            }
         }
 
         if (Input.GetKeyDown("left"))
         {
             transform.eulerAngles = Vector3.zero;
-            GetComponent<SpriteRenderer>().color = Color.red;
             attackLeft.SetActive(true);
-        }else 
-        {
-            GetComponent<SpriteRenderer>().color = Color.white;
-            attackLeft.SetActive(false);
+            attackRight.SetActive(false);
+
+            if (xPosition != 0)
+            {
+                transform.position = transform.position + new Vector3(-8, 0, 0);
+                xPosition -= 1;
+
+            }
+
         }
 
         if (Input.GetKeyDown("right"))
         {
             transform.eulerAngles = new Vector3(0, 180, 0);
-            GetComponent<SpriteRenderer>().color = Color.red;
             attackRight.SetActive(true);
-        }else
-        {
-            GetComponent<SpriteRenderer>().color = Color.white;
-            attackRight.SetActive(false);
+            attackLeft.SetActive(false);
+
+            if (xPosition != 1)
+            {
+                transform.position = transform.position + new Vector3(8, 0, 0);
+                xPosition += 1;
+
+            }
         }
     }
 
