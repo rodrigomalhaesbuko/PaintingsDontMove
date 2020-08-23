9afc8acefb4b27639e9094db481043e26fc19169
Giovanni Severo <giosvro@gmail.com>
2020-08-23 00:44:32 -0300

    Set Enemy Generator Position + Spawn Time

--- PaintingsDontMove/Assets/Scripts/Controllers/PlayerController.cs
+++ PaintingsDontMove/Assets/Scripts/Controllers/PlayerController.cs
@@ -51,7 +51,7 @@ public class PlayerController : MonoBehaviour
 
             if (xPosition != 0)
             {
-                transform.position = transform.position + new Vector3(-8, 0, 0);
+                transform.position = transform.position + new Vector3(-6, 0, 0);
                 xPosition -= 1;
 
             }
@@ -66,7 +66,7 @@ public class PlayerController : MonoBehaviour
 
             if (xPosition != 1)
             {
-                transform.position = transform.position + new Vector3(8, 0, 0);
+                transform.position = transform.position + new Vector3(6, 0, 0);
                 xPosition += 1;
 
             }
